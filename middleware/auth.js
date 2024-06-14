77eff25a624cae0807503ea9ea647d94437b8ec2
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-06-14 19:25:07 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
-
 const secretKey = 'i will become great developer';
-
 function checkToken(req,res,next){
     try{
         let  token = req.headers.authorization;
@@ -9,9 +7,7 @@ function checkToken(req,res,next){
             token = token.split(" ")[1];
             let user = jwt.verify(token,secretKey);
             req.userid = user.id;
-            console.log('middleware is working req.userid set',req.userid);
-          
-            
+            console.log('middleware is working req.userid set',req.userid)   
         }else{
             res.status(400).json({
                 message: "send a token you front end enginner. just kidding i am front end developer",
@@ -19,15 +15,11 @@ function checkToken(req,res,next){
             });
         }
         next();
-
     }catch(e){
-
         res.status(400).json({
             message: "authorization failed"
         });
-
     }
 }
-
 
 module.exports = checkToken;
